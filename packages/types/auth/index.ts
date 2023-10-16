7801e492e2e8fdb97ebd46d61e4bd68b2f97b4cb
kyle belle <43228761+kyle-belle@users.noreply.github.com>
2023-10-16 15:01:52 -0400

    remove unnecessary code for example

--- packages/types/auth/index.ts
+++
@@ -1,6 +0,0 @@
-import { checkAuth, signInHandler, signOutHandler, signUpHandler } from "@app/api/src/express/handlers/auth";
-
-export type SignInResponse = Awaited<ReturnType<typeof signInHandler>>;
-export type SignUpResponse = Awaited<ReturnType<typeof signUpHandler>>;
-export type SignOutResponse = Awaited<ReturnType<typeof signOutHandler>>;
-export type checkAuthResponse = Awaited<ReturnType<typeof checkAuth>>;
--- packages/types/user/index.ts
+++ packages/types/user/index.ts
@@ -1,5 +1,3 @@
-import type { UserType, AuthProvider } from ".prisma/client";
-
 export type UserAuthIdType = 'email';
 export type UserAuthType = 'password';
 
@@ -8,12 +6,8 @@ export interface UserCreateType {
     firstname: string;
     lastname: string;
     password?: string;
-    type: UserType;
     photoUrl?: string;
     birthdate?: Date;
-    provider?: AuthProvider;
-    providerId?: string;
-    phone?: {prefix: string, number: string}
 }
 
 export interface UserUpdateType {
@@ -23,7 +17,5 @@ export interface UserUpdateType {
     lastname?: string;
     password?: string;
     hasPassword?: boolean;
-    type?: UserType;
     birthdate?: Date;
-    phones?: ({prefix: string; number: string, primary: boolean} & ({id: number, delete?: boolean, new?: false} | {new: true}))[]
 }
